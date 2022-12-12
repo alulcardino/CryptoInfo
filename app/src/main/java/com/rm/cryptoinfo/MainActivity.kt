afbd92ea0408be3df7b8ca706308153b2a617384
alulcardino <52010205+alulcardino@users.noreply.github.com>
2022-12-12 18:56:19 +0600

    api service and factory are added

--- app/src/main/java/com/rm/cryptoinfo/MainActivity.kt
+++ app/src/main/java/com/rm/cryptoinfo/MainActivity.kt
@@ -2,10 +2,33 @@ package com.rm.cryptoinfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.rm.cryptoinfo.api.ApiFactory
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+    private val compositeDisposable = CompositeDisposable()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val disposable = ApiFactory.apiService.getFullPriceList(fSyms = "BTC,ETH,EOS")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("MyLog",  it.toString())
+            },{
+                Log.d("MyLog",  it.message.toString())
+
+            })
+        compositeDisposable.add(disposable)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 }
